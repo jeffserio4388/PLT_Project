3f177ba576d153f7aed278f30b7f53bbd972ac1a
brandonbakhshai <brandon.bakhshai@columbia.edu>
2017-03-22 18:45:52 -0400

    writing codegen

--- pipeline/src/codegen.ml
+++ pipeline/src/codegen.ml
@@ -1,41 +1,48 @@
+(* Code generation: translate takes a semantically checked AST and
+produces LLVM IR
+
+LLVM tutorial: Make sure to read the OCaml version of the tutorial
+
+http://llvm.org/docs/tutorial/index.html
+
+Detailed documentation on the OCaml LLVM library:
+
+http://llvm.moe/
+http://llvm.moe/ocaml/
+
+*)
+
 module A = Ast
+
 module StringMap = Map.Make(String)
 
-open Printf
+let translate (globals, functions) =
 
-(* file print related *)
-
-let file = "output.c"
-let message = "Hello!"
-  
-let () =
-  (* Write message to file *)
-  let oc = open_out file in    (* create or truncate file, return channel *)
-  fprintf oc "%s\n" message;   (* write something *)   
-  close_out oc;
-
-(* string map for storing variables *)
-
-
-(* specific grammars specified here *)
-
-let ltype_of_typ = function
-    A.Int     -> "int"
+  let ctype_of_typ = function
+      A.Int     -> "int"
     | A.Bool    -> "int"
     | A.Float   -> "float"
     | A.Char    -> "char"
     | A.Void    -> "void" in
 
+  let global_vars = 
+    let global_var global_map (typ, name) =
+      let init = StringMap.add n t global_map in 
+      List.fold_left global_var StringMap.empty globals in
 
-    (**)
+    let function_decls = 
+      let function_decl func_map fdecl = 
+        let name = fdecl.A.fname
+        and formal_types = Array.of_list (List.map (fun(t, _) -> ctype_of_typ t) decl.A.formal) 
+        in StringMap.add name (fdecl.A.typ, formal_types, fdecl) func_map in 
+        List.fold_left function_decl StringMap.empty functions in 
 
-  (* Declare each global variable; remember its value in a map *)
-  let global_vars =
-    let global_var m (t, n) =
-      let init = L.const_int (ltype_of_typ t) 0
-      in StringMap.add n (L.define_global n init the_module) m in
-    List.fold_left global_var StringMap.empty globals in
+        let build_function_body fdecl =
+          let add_local m (t, n) = 
+          
 
-  (* Declare printf(), which the print built-in function will call *)
-  let printf_t = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
-  let printf_func = L.declare_function "printf" printf_t the_module in
+
+
+
+
+
