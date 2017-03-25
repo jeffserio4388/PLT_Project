e65a9dda72e7a6964d279b2be2c4144c7aea6a9c
somya <somyavasudevan@github.com>
2017-03-25 19:55:39 -0400

    codegen

---
+++ pipeline/src/pipe.ml
@@ -0,0 +1,18 @@
+(* Pipeline, adapted from MicroC by Stephen Edwards Columbia University *)
+(* Top-level of the MicroC compiler: scan & parse the input,
+   check the resulting AST, generate LLVM IR, and dump the module *)
+
+
+type action = Ast | Codegen (* | Tast | Cppsast *)
+
+let _ =
+  let action = if Array.length Sys.argv > 1 then
+    List.assoc Sys.argv.(1) [ ("-a", Ast);
+                ("-c", Codegen); (* ("-t", Tast) *) ]
+  else Codegen in
+  let lexbuf = Lexing.from_channel stdin in
+  let program = Parser.program Scanner.token lexbuf in
+  match action with
+    Ast -> print_string (Ast.string_of_program (program))
+  | Codegen -> print_string (Codegen.convert (program))
+  (*| Tast -> Cppsast.convert_to_cppast (List.rev program)*)
