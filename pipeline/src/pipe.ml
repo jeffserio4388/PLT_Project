e06fab65a766df2e1b489641e91d96bc435253b2
bl-2633 <bl2633@columbia.edu>
2017-03-26 13:07:05 -0400

    edited pipe.ml

--- pipeline/src/pipe.ml
+++ pipeline/src/pipe.ml
@@ -3,16 +3,16 @@
    check the resulting AST, generate LLVM IR, and dump the module *)
 
 
-type action = Ast | Codegen (* | Tast | Cppsast *)
+type action = Ast | C_out (* | Tast | Cppsast *)
 
 let _ =
   let action = if Array.length Sys.argv > 1 then
     List.assoc Sys.argv.(1) [ ("-a", Ast);
-                ("-c", Codegen); (* ("-t", Tast) *) ]
-  else Codegen in
+                ("-c", C_out); (* ("-t", Tast) *) ]
+  else C_out in
   let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program Scanner.token lexbuf in
   match action with
     Ast -> print_string (Ast.string_of_program (program))
-  | Codegen -> print_string (Codegen.convert (program))
+  | C_out -> print_string (Codegen.convert (program))
   (*| Tast -> Cppsast.convert_to_cppast (List.rev program)*)
