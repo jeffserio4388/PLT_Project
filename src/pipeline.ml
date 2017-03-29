4834b7a3e2a768e62508b62f1b40fd8841367256
brandonbakhshai <brandon.bakhshai@columbia.edu>
2017-03-29 15:23:02 -0400

    building up pipeline.ml to be used in test suite

--- src/pipeline.ml
+++ src/pipeline.ml
@@ -1,22 +1,53 @@
 (* Top-level of the Pipeline compiler: scan & parse the input,
    check the resulting AST and generate output C file *)
 
-type action = Ast | Compile
+type action = Ast | Translate | Compile | Run | Compare
+
+let read_process command =
+  	let buffer_size = 2048 in
+  	let buffer = Buffer.create buffer_size in
+  	let string = Bytes.create buffer_size in
+  	let in_channel = Unix.open_process_in command in
+  	let chars_read = ref 1 in
+  	while !chars_read <> 0 do
+    	chars_read := input in_channel string 0 buffer_size;
+    	Buffer.add_substring buffer string 0 !chars_read
+  	done;
+  	ignore (Unix.close_process_in in_channel);
+  	Buffer.contents buffer
 
 let _ =
-  let action = if Array.length Sys.argv > 1 then
-    List.assoc Sys.argv.(1) [ ("-a", Ast);	(* Print the AST only *)
-			      ("-c", Compile) ] (* Generate output c file *)
-  else Compile in
-  let lexbuf = Lexing.from_channel stdin in
-  let ast = Parser.program Scanner.token lexbuf in
-  (* Semant.check ast; *)
-  match action with
-    Ast -> print_string (Ast.string_of_program ast)
-  | Compile -> Printf.fprintf (open_out "out.c") "%s" (Codegen.translate ast)
-  (* | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
-  | Compile -> let m = Codegen.translate ast in
-    Llvm_analysis.assert_valid_module m;
-    print_string (Llvm.string_of_llmodule m) *)
+    let action = 
+        if 
+            Array.length Sys.argv > 1 
+        then
+            List.assoc Sys.argv.(1) [   ("-a", Ast);	    (* Print the AST *)
+                                        ("-t", Translate);  (* Translate the program to output c file *)
+                                        ("-c", Compile);    (* Compile the output c file *)
+                                        ("-r", Run);        (* Run the program *)
+                                        ("-d", Compare) ]   (* Compare output and expected output *)
+        else 
+            Compile in
+            let programName = Sys.argv.(2) in
+            let program = open_in programName in 
+            let lexbuf = Lexing.from_channel program in
+            let ast = Parser.program Scanner.token lexbuf in
+            (* Semant.check ast; *)
+            match action with
+                Ast | Translate | Compile | Run | Compare -> print_string (Ast.string_of_program ast);
+            match action with
+                Ast -> ();
+                | Translate | Compile | Run | Compare -> let oc = open_out "out.c" in Printf.fprintf oc "%s" (Codegen.translate ast); close_out oc;
+            match action with
+                Ast | Translate -> ();
+                | Compile | Run | Compare-> ignore(read_process "gcc out.c -luv");
+            match action with
+                Ast | Translate | Compile -> ();
+                | Run | Compare -> ignore(print_string (read_process "./a.out > stdout.txt")); 
+                (* ignore(Unix.execv "./a.out" [| "./a.out"; "> stdout.txt" |]); *)
+            match action with
+                Ast | Translate | Compile | Run -> ();
+                | Compare -> ignore(Unix.execv "./a.out" [| "./a.out"; "> stdout.txt" |]);
+
 
 
