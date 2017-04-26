cc7c6c5cd4d319045fb0f0e1aa19cc343d517312
Jeffrey Serio <jjs2240@columbia.edu>
2017-04-25 22:14:05 -0400

    started sast in earnest: created basic sem_fdecl type

--- src/sast.ml
+++ src/sast.ml
@@ -1,6 +1,35 @@
 open Ast
 
-type sexpr = 
+type scope = {
+    parent_scope    : string;
+    locals          : bind list;
+}
+
+type sem_expr = 
     SLiteral of int
     | SMyStringLit of string
     | BoolLit of bool
+
+type sem_typ = UnderConstruction
+
+type sem_stmt = UnderConstruction
+
+type sem_fdecl = {
+    return_typ  : typ;
+    fname       : string;
+    formals     : bind list;
+    f_scope     : scope;
+    body        : sem_stmt list;
+}
+
+type sem_prgm = var_init list * stmt list * sem_fdecl list * pipe_decl list * struct_decl list
+
+let sem_fdecl_conv fdecl = 
+    {
+        return_typ  = fdecl.typ;
+        fname       = fdecl.fname;
+        formals     = fdecl.formals;
+        f_scope     = { parent_scope = "globals"; locals = [] };
+        body        = [];
+    }
+
