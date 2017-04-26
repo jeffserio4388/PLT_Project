1a4a8ce87b3aebb7d3a46fb8524ea8db07495926
bl-2633 <bl2633@columbia.edu>
2017-04-26 00:37:46 +0000

    chenged semant.ml

--- src/semant.ml
+++ src/semant.ml
@@ -120,7 +120,7 @@ let check (globals, stmts, functions, pipes, structs) =
                                 and rt = expr e in
         check_assign lt rt (Failure ("illegal assignment " ^ string_of_typ lt ^
 				     " = " ^ string_of_typ rt ^ " in " ^ 
-				     string_of_expr ex))
+                     string_of_expr ex));
       | Call(fname, actuals) as call -> let fd = function_decl fname in
          if List.length actuals != List.length fd.formals then
            raise (Failure ("expecting " ^ string_of_int
