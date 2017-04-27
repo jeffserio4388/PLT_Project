31a7cea86b5c5eab251dcc21142e20c87dc78fb3
bl-2633 <bl2633@columbia.edu>
2017-04-27 00:29:03 +0000

    working sement.ml

--- src/semant.ml
+++ src/semant.ml
@@ -9,7 +9,7 @@ module StringMap = Map.Make(String)
 
    Check each global variable, then check each function *)
 
-let check (globals, stmts, functions, pipes) =
+let check (globals, stmts, functions, pipes, structs) =
 
   (* Raise an exception if the given list has a duplicate *)
   let report_duplicate exceptf list =
