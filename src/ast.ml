d69f71659c605730a68e9d6044693f4281f3be10
Jeffrey Serio <jjs2240@columbia.edu>
2017-05-05 21:04:08 -0400

    added Dot to ast.ml

--- src/ast.ml
+++ src/ast.ml
@@ -1,7 +1,7 @@
 (* Abstract Syntax Tree and functions for printing it *)
 
 type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq |
-          And | Or
+          And | Or | Dot
 
 type uop = Neg | Not
 
