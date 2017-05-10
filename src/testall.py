26faa6e3165267ee07f7bf284828d162a0f4ae7d
bl-2633 <bl2633@columbia.edu>
2017-05-10 16:59:18 +0000

    fixed string return

--- src/testall.py
+++ src/testall.py
@@ -22,7 +22,7 @@ for file in testFiles:
 	print file
         stub = (file.split("."))[0]
 	#For each test file perform the test. And print pass or failure
-	runStr = './pipeline -d ../basic_tests/' + file
+        runStr = './pipeline -d ../basic_tests/' + file
 
 	diff_file = file[:-2]+'diff'
 	string = '../basic_tests/'+diff_file
