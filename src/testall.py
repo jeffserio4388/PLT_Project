74de47b70eabc204a5d3c9f16042c53951a37574
bl-2633 <bl2633@columbia.edu>
2017-05-10 18:45:39 +0000

    sample programs for test_plan

--- src/testall.py
+++ src/testall.py
@@ -7,55 +7,20 @@ if len(sys.argv)>1:
 else:
 	#Get all the files in the tests dir
 	testFiles = os.listdir('../basic_tests/')
-        #print testFiles
-	passTestFiles = [ x for x in testFiles if ( x[-2:]=='pl' and x[:5]=='test-pass')]
-        failTestFiles = [ x for x in testFiles if ( x[-2:]=='pl' and x[:9]=='test-fail')]
+	testFiles = [ x for x in testFiles if ( x[-2:]=='pl' and x[:4]=='test')]
 
-
-#print len(passTestFiles)
-#print len(failTestFiles)
-
+nof = len(testFiles)
 
 #passing and failing
 passed = []
 failed = []
 i=0
-print 'Starting the tests..\n'
+print 'Starting the tests..'
 #Proceed if make succeeds
 
-for file in passTestFiles:
+for file in testFiles:
 	print file
-        stub = (file.split('.'))[0]
-	#For each test file perform the test. And print pass or failure
-        runStr = './pipeline -d ../basic_tests/' + file
-
-	diff_file = file[:-2]+'diff'
-	string = '../basic_tests/'+diff_file
-	print string
-	#print 'Running for file : '+file +'\n'
-
-	os.system(runStr)
-
-	#print string
-	if os.path.exists(string):
-		print 'path !! ' + string
-		f = open(string)
-		s = f.readlines()
-		f.close()
-		if (len(s) == 0 ):
-			passed.append( '**** PASSED when it is supposed to pass for -- '+file+'\n')
-		else:
-                    failed.append('**** FAILED when it is supposed to pass. For file: '+file+'\n' )
-	else :
-            failed.append('**** FAILED when it is supposed to pass. For file: '+file+'\n')
-	i = i+1
-        runStr = 'rm ../basic_tests/' + file[:-3]
-        #print(runStr)
-        os.system(runStr)
-
-for file in failTestFiles:
-	print file
-        stub = (file.split('.'))[0]
+        stub = (file.split("."))[0]
 	#For each test file perform the test. And print pass or failure
 	runStr = './pipeline -d ../basic_tests/' + file
 
@@ -66,22 +31,23 @@ for file in failTestFiles:
 
 	os.system(runStr)
 
-	#print string
+	print string
 	if os.path.exists(string):
-		print 'path !! ' + string
+		print "path !! " + string
 		f = open(string)
 		s = f.readlines()
 		f.close()
 		if (len(s) == 0 ):
-			failed.append( '****FAILED - passes  when it is supposed to fail -- '+file+'\n')
+			passed.append( 'PASSED for -- '+file)
 		else:
-			failed.append('**** PASS - fails when it is supposed to '+file+'\n' )
+			failed.append('**** FAILED for file - Different output than expected '+file+'\n' )
 	else :
-		passed.append('**** PASS - failed it is suposed to fail  '+file+'\n')
+		failed.append('**** FAILED for file '+file+'\n')
 	i = i+1
         runStr = 'rm ../basic_tests/' + file[:-3]
-        #print(runStr)
+        print(runStr)
         os.system(runStr)
+
 runStr = 'rm ../basic_tests/*.c'
 os.system(runStr)
 
